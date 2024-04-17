d85f20e0e2b07a734c2a81764e7088e1f16595ef
MKEXPLOIT <95012995+MKEXPLOIT@users.noreply.github.com>
2024-04-16 22:38:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,16 @@ export default function Home() {
           Eternizando quadros.
           @prodvmk @vmkvisuals
 
+<script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+
+<stripe-buy-button
+  buy-button-id="buy_btn_1P6NPdF6QS4xmb6iPnPi1Qxi"
+  publishable-key="pk_live_51P4ZOKF6QS4xmb6iZ1cDygZCrYDJeuh0zeyRzwvg4M8JySB7Nv1nghsa8vD3BtvS1iYUFcrQtWKo8OTEq9k0YaDq00zZGdFi8L"
+>
+</stripe-buy-button>
+
         </p>
       </main>
 
