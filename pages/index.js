8dc2ba113964784c0a61930276d26b330528b053
MKEXPLOIT <95012995+MKEXPLOIT@users.noreply.github.com>
2024-04-16 23:40:46 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,6 @@ export default function Home() {
 
         </p>
       </main>
-<link rel="stylesheet" href="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-button.css">
-<a id="robbu-whatsapp-button" class="left" target="_blank" href="https://api.whatsapp.com/send?phone=5544988391421&text=Ol%C3%A1">
-  <div class="rwb-tooltip">Fale comigo!</div>
-  <img src="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-icon.svg">
-</a>
 
 <script async
   src="https://js.stripe.com/v3/buy-button.js">
