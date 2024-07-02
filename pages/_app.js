15482a43edba9e63ff0d36bce34c01b9c669db96
MKEXPLOIT <95012995+MKEXPLOIT@users.noreply.github.com>
2024-07-01 22:16:58 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,3 @@ function Application({ Component, pageProps }) {
 export default Application
 
 
-function ImageComponent() {
-  return (
-
-      <h2>fundo.png</h2>
-      <img src={exampleImage} alt="Fundo.png" />
